b0512b97e8362fa300db6697d3d0a81954ed156f
kispanchal86@gmail.com <kispanchal86@gmail.com>
2022-09-07 20:41:14 +0530

    latest api

---
+++ routesapi.js
@@ -0,0 +1,96 @@
+module.exports = function (app, sfcon) {
+    debugger;
+    console.log('sfcon::'+sfcon);
+    app.get('/',async(req,res,next)=>{
+        res.status(200).json({'message':'Own by Yovo'})
+    })
+    app.get('/api/test', async function (req, res, next) {
+        res.status(200).json({'message':'Bhaskar Jha'})
+    })
+    app.get('/api/account', async function (req, res, next) {
+        console.log('Inside accountfetch')
+        var records = [];
+        await sfcon.query("SELECT Id, Name FROM visit__c", function(err, result) {
+            if (err) {
+                console.log('err::' + err);
+                return console.error(err);
+            }
+            
+        console.log("total : " + result.totalSize);
+            console.log("fetched : ", result);
+            res.status(200).json({'message':result})
+        });
+
+    })
+    app.post('/api/authenticate_user', async function (req, res, next) {
+        console.log('Inside accountfetch')
+        var records = [];
+        await sfcon.query("SELECT Id,Password__c,Username FROM user " +
+            "WHERE Username = '" +
+            req.body.username + "' AND Password__c = '" + req.body.password + "'"
+            , function (err, result) {
+            if (err) {
+                console.log('err::' + err);
+                return console.error(err);
+            }
+            
+        console.log("total : " + result.totalSize);
+                console.log("fetched : ", result);
+                if (result.totalSize > 0) {
+                    console.log('result::' + result.records[0].Id);
+                    res.status(200).json({ 'message': result.records[0].Id,'Status':'true' })
+                } else {
+                    res.status(200).json({ 'message': 'No User Found.','Status':'false' })
+                }
+                
+                
+        });
+
+    })
+
+    app.post('/api/get_visit', async function (req, res, next) {
+        console.log('Inside accountfetch')
+        var records = [];
+        await sfcon.query("SELECT Id,Check_In__c,Check_Out__c,User__c,User__r.name,Visit_Date_Time__c,Name,Is_Visited__c FROM Visit__c WHERE User__c = '"
+            + req.body.userid + "'"
+            , function (err, result) {
+            if (err) {
+                console.log('err::' + err);
+                return console.error(err);
+            }
+            
+        console.log("total : " + result.totalSize);
+                console.log("fetched : ", result);
+                if (result.totalSize > 0) {
+                    console.log('result::' + result.records[0].Id);
+                    res.status(200).json({ 'message': result.records,'Status':'true' })
+                } else {
+                    res.status(200).json({ 'message': 'No visit available for user','Status':'false' })
+                }
+                
+                
+        });
+
+    })
+
+    app.post("/api/create_visit_record", async function (req, res, next) {
+        try {
+          console.log("err::" + JSON.stringify(req.body));
+          await sfcon
+            .sobject("visit__c")
+            .create(req.body, function (err, response) {
+              if (err) {
+                console.log("err::" + err);
+                return console.log(err);
+              }
+              console.log("response" + JSON.stringify(response));
+              res.send(JSON.stringify(response));
+            });
+        } catch (err) {
+          console.log(err);
+          next(err);
+        }
+      });
+
+
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+
+const app = express()
+const cors = require("cors");
+app.use(cors());
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+var browserSync = require('browser-sync');
+
+var bodyParser = require('body-parser');
+//var methodOverride = require('method-override');
+var jsforce = require('jsforce');
+const PORT = process.env.PORT || 5000
+app.use(bodyParser.json()); // parse application/json 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+
+//app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(express.static(__dirname + '/dist')); //public
+var conn = new jsforce.Connection({
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    loginUrl: 'https://test.salesforce.com'
+});
+conn.login('ajeet.kumar@utilitarianlab.com.sitevisit', 'ajeet@123', function(err, userInfo) {
+    if (err) {
+        console.log('Errorr::'+err);
+         return console.error(err); }
+    // Now you can get the access token and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.instanceUrl);
+    // logged in user property
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    // ...
+});
+console.log('conn::' + conn);
+require('./routesapi')(app, conn);
+
+
+
+app.listen(PORT, () => { console.log(`Server running at http://localhost:` + PORT) })
+exports = module.exports = app;
